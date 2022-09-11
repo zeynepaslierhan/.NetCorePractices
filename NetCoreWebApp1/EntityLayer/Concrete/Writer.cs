5e65852e6e6b58464c45defc455dfe59c850e1be
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-09-11 04:10:04 +0300

    Katmanlar oluşturuldu, Class'lar eklenmeye başlandı

---
+++ NetCoreWebApp1/EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterAbout { get; set; }
+        public string WriteImage { get; set; }
+        public string WritePassword { get; set; }
+        public bool WriterStatus { get; set; }
+    }
+}

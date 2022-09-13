6b8e6fb62a7e8683fcea29f6d2e2a190fa4a2a09
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-09-14 01:25:09 +0300

    Tablolar arası ilişkiye başlandı ve ınterface ile repositoy işlemleri başladı
    Blog tablosu ile comment ve category ilişkisi sağlandı.
    
    Generic yapı oluşturmak için IGenericDal ınterface'i ile GenericRepository oluşturuldu.

---
+++ NetCoreWebApp1/DataAccessLayer/Migrations/20220913220049_BlogveCategoryİlişkisiKuruldu.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class BlogveCategoryİlişkisiKuruldu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_blogs_CategoryID",
+                table: "blogs",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_blogs_categories_CategoryID",
+                table: "blogs",
+                column: "CategoryID",
+                principalTable: "categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_blogs_categories_CategoryID",
+                table: "blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_blogs_CategoryID",
+                table: "blogs");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "blogs");
+        }
+    }
+}
---
+++ NetCoreWebApp1/DataAccessLayer/Migrations/20220913220402_BlogveCommentİlişkisiKuruldu.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class BlogveCommentİlişkisiKuruldu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlogID",
+                table: "comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_BlogID",
+                table: "comments",
+                column: "BlogID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_comments_blogs_BlogID",
+                table: "comments",
+                column: "BlogID",
+                principalTable: "blogs",
+                principalColumn: "BlogID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_comments_blogs_BlogID",
+                table: "comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_comments_BlogID",
+                table: "comments");
+
+            migrationBuilder.DropColumn(
+                name: "BlogID",
+                table: "comments");
+        }
+    }
+}

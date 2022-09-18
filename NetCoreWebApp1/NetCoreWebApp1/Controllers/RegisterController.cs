ad33ebae53e7ae514e45b134409493250956d7d2
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-09-18 16:41:11 +0300

    Register sayfası için işlemler başladı
    Business Layerda IWriter, WriterManager oluşşturuldu.
    UI layerda RegisterController oluşturuldu.
    Register view düzenlendi
    sweetalert kullanıldı

---
+++ NetCoreWebApp1/NetCoreWebApp1/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApp1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer w)
+        {
+            w.WriterStatus = true;
+            w.WriterAbout = "Deneme";
+            wm.WriterAdd(w);
+
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}

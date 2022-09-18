be0d8b932301778c1a66e8042a39cbb64ece9abf
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-09-18 18:16:08 +0300

    Validation işlemleri başladı.
    Business Layer'a gerekli nuget packages eklenip WriterValidator oluşturuldu

---
+++ NetCoreWebApp1/BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            //Writer Name
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar adı soyadı maksimum 50 karakter olabilir.");
+            //Writer Password
+            RuleFor(x => x.WritePassword).NotEmpty().WithMessage("Şifre boş geçilemez");
+            //Writer Mail
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
+        }
+    }
+}

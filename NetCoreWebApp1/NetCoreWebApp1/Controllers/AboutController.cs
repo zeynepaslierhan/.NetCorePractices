9d65d61097a9d7e36f2f78b13c9dc20bd5662b69
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-10-03 23:26:29 +0300

    About Sayfası

---
+++ NetCoreWebApp1/NetCoreWebApp1/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApp1.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        AboutManager aboutManager = new AboutManager(new EFAboutRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}

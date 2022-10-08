36051e6e6b888af3ee6da0398ab234f5a2627879
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-10-08 18:26:03 +0300

    Minik Düzenlemeler
    statup dosyasında default olarak Blog sayfasının uzantısı verildi.
    Layout düzenlenip butonların sayfalarına erişim sağlandı.
    Kullanılmayan layout silindi.
    Layout'un alt kısmına (Partial3) oluşturduğumuz component ve partial viewlar eklendi.
    blogLastThreePost içerisi düzenlendi
    ContactController ve sayfası oluşturuldu ama daha içeriği düzenlenmedi.

--- NetCoreWebApp1/NetCoreWebApp1/Startup.cs
+++ NetCoreWebApp1/NetCoreWebApp1/Startup.cs
@@ -52,7 +52,7 @@ namespace NetCoreWebApp1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }

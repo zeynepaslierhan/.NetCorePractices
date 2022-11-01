37fd9cdb0aa8f6719d2a1baff898749562bb7167
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-11-02 02:25:26 +0300

    Controller seviyesinde Login işlemi
    Startup için gerekli kodlar
    LoginController
    WriterController

--- NetCoreWebApp1/NetCoreWebApp1/Controllers/LoginController.cs
+++ NetCoreWebApp1/NetCoreWebApp1/Controllers/LoginController.cs
@@ -1,16 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NetCoreWebApp1.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Writer w)
+        {
+            Context c = new Context();
+
+            var dataValue = c.writers.FirstOrDefault(x => x.WriterMail == w.WriterMail && x.WritePassword == w.WritePassword);
+
+            if(dataValue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, w.WriterMail)
+                };
+                var useridentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index","Writer");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }

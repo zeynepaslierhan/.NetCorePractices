e3a2cda03e6e1f08a4ac07e47fcf1b0d7760fb7d
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-11-05 21:21:28 +0300

    Yazar panelinden yeni blog oluşturma

--- NetCoreWebApp1/NetCoreWebApp1/Controllers/WriterController.cs
+++ NetCoreWebApp1/NetCoreWebApp1/Controllers/WriterController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace NetCoreWebApp1.Controllers
     public class WriterController : Controller
     {
         BlogManager bm = new BlogManager(new EFBlogRepository());
+        CategoryManager cm = new CategoryManager(new EFCategoryRepository());
         public IActionResult Index()
         {
             return View();
@@ -30,6 +33,29 @@ namespace NetCoreWebApp1.Controllers
         {
             var values = bm.GetListWithCategoryByWriter(7);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+
+            List<SelectListItem> categories = (from x in cm.GetList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryID.ToString()
+                                               }).ToList();
+            ViewBag.cv = categories;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog b)
+        {
+            b.BlogStatus = true;
+            b.WriterId = 7; //şimdilik
+            bm.TAdd(b);
+            return RedirectToAction("BlogListByWriter", "Writer");
         }
     }
 }
